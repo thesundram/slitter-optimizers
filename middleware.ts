2907701397acfff14fbe0eecdca83791e9756da9
Sundram Pandey <sundrampandey347@gmail.com>
2026-01-14 22:48:54 +0530

    updates

--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,10 @@
-// import { withAuth } from 'next-auth/middleware';
-
-// export default withAuth({
-//   pages: {
-//     signIn: '/login',
-//   },
-// });
-
-// export const config = {
-//   matcher: ['/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)'],
-// };
-// middleware.ts
 import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
 
-export default withAuth(
-  function middleware(req) {
-    const token = req.nextauth?.token;
-    
-    if (token?.isExpired) {
-      const loginUrl = new URL('/login', req.url);
-      loginUrl.searchParams.set('error', 'Account expired');
-      return NextResponse.redirect(loginUrl);
-    }
-    
-    return NextResponse.next();
+export default withAuth({
+  pages: {
+    signIn: '/login',
   },
-  {
-    callbacks: {
-      authorized: ({ token }) => {
-        return !!token && !token?.isExpired;
-      },
-    },
-    pages: {
-      signIn: '/login',
-    },
-  }
-);
+});
 
 export const config = {
   matcher: ['/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)'],
