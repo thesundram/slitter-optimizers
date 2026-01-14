5991a157e757a7367070b35413d1bf6115ca8225
Sundram Pandey <sundrampandey347@gmail.com>
2026-01-14 22:00:03 +0530

    Fix middleware for Next.js 16

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,11 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth({
+  pages: {
+    signIn: '/login',
+  },
+});
 
 export const config = {
-  matcher: ['/((?!login|api/auth).*)'],
+  matcher: ['/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)'],
 };
